010a133a98a8fc10d3ae58e09068edd472a12a81
Colt Dixon <mcoltdixon@gmail.com>
2022-05-26 17:33:02 -0500

    added some filtering on ancestries

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends React.Component {
     super(props)
 
     this.state = {
+      classList: [],
+      bgList: [],
+      ancestList: [],
       charClass: null,
       background: null,
       ancestry: null,
@@ -17,12 +20,25 @@ class App extends React.Component {
       lastAncesIndex: null,
     }
   }
+  
+  componentDidMount() {
+    this.setState({classList: classes, bgList: backgrounds, ancestList: ancestries})
+  }
 
   handleGenerateCharacter() {
-    const randomClassIndex = this.randomIndex(classes.length, "classes")
-    const randomBGIndex = this.randomIndex(backgrounds.length, "backgrounds")
-    const randomAncesIndex = this.randomIndex(ancestries.length, "ancestries")
-    this.setState({charClass: classes[randomClassIndex], background: backgrounds[randomBGIndex], ancestry: ancestries[randomAncesIndex]})
+    const randomClassIndex = this.randomIndex(this.state.classList.length, "classes")
+    const randomBGIndex = this.randomIndex(this.state.bgList.length, "backgrounds")
+    const randomAncesIndex = this.randomIndex(this.state.ancestList.length, "ancestries")
+    this.setState({charClass: classes[randomClassIndex], background: backgrounds[randomBGIndex], ancestry: this.state.ancestList[randomAncesIndex]})
+  }
+
+  handleRarity(e, type) {
+    if (e.target.checked) {
+      const newAncestList = this.state.ancestList.filter(ancestry => ancestry.rarity !== type)
+      this.setState({ancestList: newAncestList})
+    } else {
+      this.setState({ancestList: ancestries})
+    }
   }
 
   randomIndex(length, type) {
@@ -51,11 +67,15 @@ class App extends React.Component {
     return (
       <div className="container">
         <h1>Quick &amp; Dirty Pathfinder 2E Random Generator</h1>
-        <button onClick={() => {this.handleGenerateCharacter()}}>Generate Character!</button>
+        <div className="form-box">
+          <input onChange={(e) => {this.handleRarity(e, "uncommon")}} id="uncommon" type="checkbox"/><label htmlFor="uncommon">Exclude Uncommon Ancestries</label><br />
+          <input onChange={(e) => {this.handleRarity(e, "rare")}} id="rare" type="checkbox"/><label htmlFor="rare">Exclude Rare Ancestries</label><br />
+          <button onClick={() => {this.handleGenerateCharacter()}}>Generate Character!</button>
+        </div>
 
         <div className="character">
           <div id="ancestry-info" className="info">
-            <strong>Ancestry:</strong> {this.state.ancestry}
+            <strong>Ancestry:</strong> {this.state.ancestry?.name}
           </div>
           <div id="background-info" className="info">
             <strong>Background:</strong> {this.state.background}
